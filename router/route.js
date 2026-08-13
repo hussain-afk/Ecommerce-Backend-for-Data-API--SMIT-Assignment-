b0739b8e7815359f9d79cfe6de1f0144f5e9e427
hussain kadir memon <hussainkadir245@gmail.com>
2026-08-14 01:31:51 +0500

    comments data added

--- router/route.js
+++ router/route.js
@@ -17,4 +17,36 @@ router.get("/products", async (req, res) => {
     }
 });
 
+router.get("/comments", async (req, res) => {
+    const dbPath = path.join(process.cwd(), "database", "comments.json");
+    try {
+        const data = await fs.promises.readFile(dbPath, "utf-8");
+        const comments = await JSON.parse(data);
+        res.json(comments);
+    } catch (error) {
+        console.error("Error reading database:", error);
+        res.status(500).send("Error reading database");
+    }
+});
+
+
+router.post("/post/review", async (req, res) => {
+    const dbPath = path.join(process.cwd(), "database", "comments.json");
+    try {
+        const data = {
+            id: Date.now(),
+            name: req.body.name,
+            comment: req.body.comment,
+            rating: req.body.rating,
+            date: new Date().toString(),
+        }
+        await fs.promises.writeFile(dbPath, JSON.stringify(data));
+        res.status(201).send("Review posted successfully");
+    } catch (error) {
+        console.error("Error posting review:", error);
+        res.status(500).send("Error posting review");
+    }
+
+});
+
 export default router;
